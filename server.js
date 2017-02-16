d121b23584d40517ff9fa06cfafb58e5a20c28f6
gaorock530@hotmail.com <gaorock530@hotmail.com>
2017-02-17 03:41:19 +0800

    x

--- server.js
+++ server.js
@@ -4,14 +4,13 @@ var app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use((req, res, next) => {
-  //console.log(req.headers);
+  //console.log(req.url);
 
   if (req.headers['x-forwarded-proto'] === 'http') {
     next();
   }else{
-    res.redirect(`http://${req.hostname}${req.url}`);
+    res.redirect("http://"+req.hostname+req.url);
   }
-  next();
 });
 
 app.use(express.static('public'));
