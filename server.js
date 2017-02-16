d2afaca30764f892b9ef60988642f9792cc93694
gaorock530@hotmail.com <gaorock530@hotmail.com>
2017-02-17 03:30:50 +0800

    middleware

--- server.js
+++ server.js
@@ -4,11 +4,14 @@ var app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use((req, res, next) => {
+  //console.log(req.headers);
+
   if (req.headers['x-forwarded-proto'] === 'http') {
     next();
   }else{
     res.redirect(`http://${req.hostname}${req.url}`);
   }
+  next();
 });
 
 app.use(express.static('public'));
